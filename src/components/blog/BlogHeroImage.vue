529d63f2615b6164d14affe4c83074da64cfddf5
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-09-07 21:15:40 +0100

    Added categories.json, added blog hero image component

---
+++ src/components/blog/BlogHeroImage.vue
@@ -0,0 +1,20 @@
+<template>
+    <img :src="getHeroImage"/>
+</template>
+
+<script>
+export default {
+    name: 'BlogHeroImage',
+    props: {
+        imageUrl: {
+            required: true,
+            type: String
+        }
+    },
+    computed: {
+        getHeroImage() {
+            return require('@/assets/blog/' + this.imageUrl);
+        }
+    }
+}
+</script>
--- src/components/blog/BlogSinglePage.vue
+++ src/components/blog/BlogSinglePage.vue
@@ -1,16 +1,19 @@
 <template>
-    <div v-if="posts.length > 0">
+    <div v-if="hasPosts">
         <div v-html="post.title"></div>
+        <blog-hero-image v-if="post.image" :image-url="post.image"/>
         <div v-html="post.intro"></div>
         <div v-html="post.content"></div>
     </div>
 </template>
 
 <script>
-import BlogJson from "@/content/blog/blog.json";
+import BlogJson from "@/content/blog/posts.json";
+import BlogHeroImage from "@/components/blog/BlogHeroImage";
 
 export default {
     name: 'BlogSinglePage',
+    components: {BlogHeroImage},
     data () {
         return {
             slug: null,
@@ -19,9 +22,14 @@ export default {
     },
     computed: {
         post () {
-            return this.posts.filter(post => {
-                return post.slug === this.slug;
-            })[0];
+            for (let key in this.posts) {
+                if (key === this.slug) {
+                    return this.posts[key];
+                }
+            }
+        },
+        hasPosts() {
+            return Object.keys(this.posts).length > 0;
         }
     },
     mounted () {
